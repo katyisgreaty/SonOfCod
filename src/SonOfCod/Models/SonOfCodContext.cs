859a98a6f9f29bb8a5e579b479b974ee277f0d5f
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-04-28 09:19:40 -0700

    begin project, add partial nonfunctioning migrations and user authentication

---
+++ src/SonOfCod/Models/SonOfCodContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SonOfCod.Models
+{
+    public class SonOfCodContext : IdentityDbContext<AdminUser>
+    {
+        public SonOfCodContext()
+        {
+        }
+
+        public DbSet<Subscriber> Subscribers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SonOfCod;integrated security=True");
+        }
+
+        public SonOfCodContext(DbContextOptions<SonOfCodContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/SonOfCod/Models/Subscriber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SonOfCod.Models
+{
+    public class Subscriber
+    {
+        [Key]
+        public int SubscriberId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string FaveSeaCreature { get; set; }
+
+    }
+}
